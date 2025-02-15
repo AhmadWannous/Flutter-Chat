be0f5aba605a4d7a1cdb76818346a5958e5ac760
Ahmad wannous <ahmadwanous642@gmail.com>
2025-02-15 18:52:46 +0200

    Signing Users Up

--- lib/screen/auth.dart
+++ lib/screen/auth.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:firebase_auth/firebase_auth.dart';
+
+final _firebase = FirebaseAuth.instance;
+
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
 
@@ -16,11 +20,34 @@ class _AuthScreenState extends State<AuthScreen> {
   var _entredEmail = '';
   var _entredPassword = '';
 
-  void _submit() {
+  void _submit() async {
     final isValid = _form.currentState!.validate();
 
-    if (isValid) {
-      _form.currentState!.save();
+    if (!isValid) {
+      return;
+    }
+
+    _form.currentState!.save();
+
+    if (_isLogin) {
+      //log users in
+    } else {
+      try {
+        final userCredentials = await _firebase.createUserWithEmailAndPassword(
+            email: _entredEmail, password: _entredPassword);
+        print(userCredentials);
+      } on FirebaseAuthException catch (error) {
+        if (error.code == 'email-already-in-use') {
+          //....
+        }
+        ScaffoldMessenger.of(context).clearSnackBars();
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(error.message ??
+                'Authentication failed.'), //if is null return 'message'
+          ),
+        );
+      }
     }
   }
 
